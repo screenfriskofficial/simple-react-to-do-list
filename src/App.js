c621ab124e6ee62d345788c74b3adf23302d7d62
Mark Che <screenfriskofficial@gmail.com>
2022-12-04 20:09:44 +1100

    create html and css for this project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './scss/App.scss';
+import React from 'react';
 function App() {
+  const [tasks, setDeleteTask] = React.useState([
+    {
+      id: 0,
+      title: 'hello world',
+    },
+    {
+      id: 1,
+      title: 'hello world1',
+    },
+    {
+      id: 2,
+      title: 'hello world2',
+    },
+  ]);
+  const deleteTaskHandler = (index) => {
+    setDeleteTask(tasks.filter((t) => t.id !== index));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="header">
+          <h1 className="header__title">react-to-do-list</h1>
+        </header>
+        <section className="content">
+          <form>
+            <input type="text" />
+            <input type="text" />
+            <button className="create-btn">Create task</button>
+          </form>
+          <div className="tasks">
+            <ul>
+              {tasks.map((task, index) => (
+                <li>
+                  <p>{task.title}</p>
+                  <button onClick={() => deleteTaskHandler(index)}>delete</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
